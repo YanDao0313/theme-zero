99ccb292178dc5467637b7dc2f524d8ee386f7e6
chanshiyu <1124590931@qq.com>
2022-05-09 15:15:53 +0800

    add transition

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
 import React from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { CSSTransition, SwitchTransition } from 'react-transition-group'
 import './App.css'
 import Side from '@components/Side'
 import Cloud from '@components/Cloud'
+import Home from '@/pages/Home'
+import About from '@/pages/About'
+
+const ZeroRoutes = () => {
+  const location = useLocation()
+  return (
+    <SwitchTransition mode="out-in">
+      <CSSTransition
+        key={location.pathname}
+        classNames="fade-in"
+        appear
+        in
+        addEndListener={(node, done) => node.addEventListener('transitionend', done, false)}
+      >
+        <Routes location={location}>
+          <Route path={'/home'} element={<Home />} />
+          <Route path={'/about'} element={<About />} />
+        </Routes>
+      </CSSTransition>
+    </SwitchTransition>
+  )
+}
 
 const App = () => {
   return (
     <div className="app">
       <Cloud />
       <Side />
+      <ZeroRoutes />
     </div>
   )
 }
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,153 @@
+// import React, { useState, useEffect, useRef, MouseEvent } from 'react'
+// import { useHistory } from 'react-router-dom'
+// import clsx from 'clsx'
+// import { Issue } from '@/type'
+// import Loading from '@components/Loading'
+// import Markdown from '@/components/Markdown'
+// import { Calendar, Bookmark, Tag } from '@components/Icons'
+// import { queryIssues } from '@utils/service'
+// import { formatIssue } from '@utils/format'
+// import { useLoading } from '@/utils/hook'
+// import styles from './index.module.css'
+
+// type HomeProps = {}
+
+// const Home: React.FC<HomeProps> = () => {
+//   const history = useHistory()
+//   const delay = useLoading()
+//   const [page, setPage] = useState(1)
+//   const [issues, setIssues] = useState<Array<Issue>>([])
+//   const maskRef = useRef<HTMLDivElement>(null)
+//   const listRef = useRef<HTMLDivElement>(null)
+//   const hoverRef = useRef<any>(null)
+//   const timerRef = useRef<number>()
+//   const loadingRef = useRef<boolean>(false)
+//   const finishedRef = useRef<boolean>(false)
+//   const [maskHeight, setMaskHeight] = useState(0)
+//   const [maskTop, setMaskTop] = useState(0)
+
+//   const handleQuery = () => {
+//     loadingRef.current = true
+//     queryIssues(page)
+//       .then(async (data) => {
+//         if (page === 1) {
+//           await delay()
+//         }
+
+//         if (data.length) {
+//           data = data.map(formatIssue)
+//           setIssues([...issues, ...data])
+//         } else {
+//           finishedRef.current = true
+//         }
+
+//         if (maskHeight === 0) {
+//           setTimeout(() => {
+//             const target = listRef.current?.firstChild
+//             if (target) {
+//               calcMaskPos(target)
+//             }
+//           }, 100)
+//         }
+//       })
+//       .finally(() => {
+//         loadingRef.current = false
+//       })
+//   }
+
+//   useEffect(() => {
+//     handleQuery()
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [page])
+
+//   const calcMaskPos = (target: any) => {
+//     const { clientHeight, offsetTop } = target
+//     const realTop = offsetTop + 3 * 16 // 3rem padding
+//     if (maskHeight === clientHeight && maskTop === realTop) return
+//     setMaskHeight(clientHeight)
+//     setMaskTop(realTop)
+//   }
+
+//   const handleMask = (e: MouseEvent<HTMLElement>) => {
+//     e.preventDefault()
+//     hoverRef.current = e.currentTarget
+//     calcMaskPos(e.currentTarget)
+//   }
+
+//   const handleScroll = () => {
+//     clearTimeout(timerRef.current)
+//     timerRef.current = window.setTimeout(() => {
+//       if (hoverRef.current) {
+//         calcMaskPos(hoverRef.current)
+//       }
+//     }, 100)
+
+//     // load more
+//     if (loadingRef.current || finishedRef.current) return
+//     const { scrollTop, scrollHeight, clientHeight } = document.documentElement
+//     if (scrollTop + clientHeight > scrollHeight - 100) {
+//       setPage((page) => page + 1)
+//     }
+//   }
+
+//   useEffect(() => {
+//     window.addEventListener('scroll', handleScroll, false)
+//     return () => window.removeEventListener('scroll', handleScroll, false)
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [])
+
+//   return (
+//     <div className="page">
+//       {issues.length === 0 ? (
+//         <Loading />
+//       ) : (
+//         <div className="fade select-text">
+//           <div
+//             ref={maskRef}
+//             className={clsx(
+//               styles.mask,
+//               'pointer-events-none absolute top-0 left-0 w-full rounded transform transition-all ease-in-out duration-300',
+//             )}
+//             style={{
+//               height: `${maskHeight}px`,
+//               transform: `translateY(${maskTop}px)`,
+//             }}
+//           ></div>
+//           <div ref={listRef} className="relative z-10 space-y-4">
+//             {issues.map((issue) => {
+//               return (
+//                 <article
+//                   key={issue.id}
+//                   className="cursor-pointer p-4 tracking-wide"
+//                   onClick={() => history.push(`/post/${issue.number}`)}
+//                   onMouseOver={handleMask}
+//                   onMouseEnter={handleMask}
+//                 >
+//                   <h3 className="text-xl italic mb-2">{issue.title}</h3>
+//                   <Markdown content={issue.description} />
+//                   <div className="flex justify-start mt-2">
+//                     <Calendar className="mr-0.5" />
+//                     {issue.created_at}
+//                     <Bookmark className="ml-4 mr-0.5" />
+//                     {issue.milestone ? issue.milestone.title : '未分类'}
+//                     <Tag className="ml-4 mr-0.5" />
+//                     {issue.labels.map((label) => (
+//                       <span key={label.id}>{label.name}</span>
+//                     ))}
+//                   </div>
+//                 </article>
+//               )
+//             })}
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   )
+// }
+// export default Home
+
+import React from 'react'
+
+const Home = () => <div>home</div>
+
+export default Home
