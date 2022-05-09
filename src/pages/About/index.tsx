ad804806a61cf73e8132ee07382caf9d9cdcc804
chanshiyu <1124590931@qq.com>
2022-05-10 00:58:54 +0800

    genshin loading

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import Genshin from '@components/Genshin'
 
-const About = () => <div>About</div>
+const About = () => (
+  <div>
+    <Genshin />
+  </div>
+)
 
 export default About
