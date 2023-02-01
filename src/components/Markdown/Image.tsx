f7bab7e715dcd2149e96a4decf9ce7946c1a23fa
chanshiyu <me@chanshiyu.com>
2023-02-01 09:54:19 +0800

    update:优e:优化图片加载

--- src/components/Markdown/Image.tsx
+++ src/components/Markdown/Image.tsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useLayoutEffect } from 'react'
 import { ComponentPropsWithoutRef, ComponentType, ReactMarkdownProps } from 'react-markdown/lib/ast-to-react'
-import clsx from 'clsx'
 import Spinner from '@/components/Spinner'
 import { fileCDN } from '@/utils'
 import { useLoading } from '@/utils/hook'
@@ -10,23 +9,23 @@ type ImageProps = ComponentPropsWithoutRef<'img'> & ReactMarkdownProps
 type ImageComponent = ComponentType<ImageProps>
 
 const Image: ImageComponent = ({ src = '', alt = '' }) => {
+  const [cdnSrc] = useState<string>(() => fileCDN(src))
   const [loading, setloading] = useState(true)
   const delay = useLoading()
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const img = new window.Image()
     img.onload = async () => {
       await delay()
       setloading(false)
     }
-    img.src = fileCDN(src)
+    img.src = cdnSrc
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [src])
+  }, [cdnSrc])
 
   return (
     <>
-      <img className={clsx('img-zoomable m-auto fade rounded shadow-md', loading && 'hidden')} src={src} alt={alt} />
-      {loading && <Spinner />}
+      {loading ? <Spinner /> : <img className="img-zoomable m-auto fade rounded shadow-md" src={cdnSrc} alt={alt} />}
       {alt && <span className="block mt-2 text-center italic">◭ {alt}</span>}
     </>
   )
